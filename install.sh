5f7093252f0099fb5d4c41f9c6f1e266231a5dc6
Albin Mattsson <albin@mattsson.io>
2017-01-23 18:57:46 +0100

    🔥 Only install Ansible if missing

--- install.sh
+++ install.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-# Make sure homebrew is installed first
+# Make sure homebrew is installed
 if [[ ! "$(type -P brew)" ]]; then
-    echo "Installing Homebrew"
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  echo "Installing Homebrew"
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-brew install ansible
+# Make sure Ansible is installed
+if [[ ! "$(type -P ansible)" ]]; then
+  echo "Installing Ansible"
+  brew install ansible
+fi
 
 ansible-playbook playbook.yml
